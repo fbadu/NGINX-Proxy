166764f872ab0bad45d3ce29ec8a6c4cc100894c
fbadu <fadliyantob@gmail.com>
2023-01-25 12:20:14 +0800

    change url remote

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log(`\n Server listening on port ${port} \n`)
